3645c57683e20ddde500a50dbad29c6033ecd649
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-17 15:32:56 +0100

    fetch countries

--- src/api/index.js
+++ src/api/index.js
@@ -29,3 +29,14 @@ export const fecthDailyData = async () => {
         
     }
 }
+
+export const fetchCountries = async () => {
+    try {
+        
+    const {data : {countries}} = await axios.get(`${url}/countries`)
+        
+     return countries.map(country =>  country.name)
+    } catch (error) {
+        
+    }
+}
