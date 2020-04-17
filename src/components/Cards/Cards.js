ed5d6db1af553e03ee8b999ff1f8945c599feed4
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-17 21:12:20 +0100

    spinner ~ loading

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -3,10 +3,11 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 import CountUp from 'react-countup'
 import cx from 'classnames'
 import styles from './Cards.module.css'
+import Spinner from './Spinner'
 
 const Cards = ( {data: {confirmed, recovered, deaths, lastUpdate}}) => {
  if (!confirmed){
-   return 'Loading...'
+   return <Spinner/>
  }
   return (
     <div className={styles.container}>
